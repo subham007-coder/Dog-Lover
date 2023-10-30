0d321995a1d318bba41d7417882af1a095d02736
subham007-coder <dsubham109@gmail.com>
2023-10-30 20:55:29 +0530

    add

---
+++ script.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("#clickForResult");
+
+let result = document.querySelector("#res")
+
+let url = "https://catfact.ninja/fact";
+
+async function getRquest(){
+    try {
+        let res = await axios(url);
+        console.log(res.data.fact);
+        result.innerHTML += `${res.data.fact} <br> <br>`;
+    } catch (err) {
+        console.log(`Error is ${err}`);
+        result.innerHTML = `<h1>Page Not Found</h1>${err}`;
+    }
+}
+
+btn.addEventListener("click", () => {
+    getRquest();
+})
