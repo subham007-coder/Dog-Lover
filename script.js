8b00c7e54ab15301eeefbd7568aec251315a8540
subham007-coder <dsubham109@gmail.com>
2023-10-30 21:14:12 +0530

    done

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ let result = document.querySelector("#res")
 
 let url = "https://dog.ceo/api/breeds/image/random";
 
-async function getRquest(){
+async function getRquest() {
     try {
         let res = await axios(url);
-        console.log(res.data.message
-            );
-        result.innerHTML += `${res.data.message} <br> <br>`;
+        console.log(res.data.message);
+        return res.data.message;
     } catch (err) {
         console.log(`Error is ${err}`);
         result.innerHTML = `<h1>Page Not Found</h1>${err}`;
     }
 }
 
-btn.addEventListener("click", () => {
-    getRquest();
+btn.addEventListener("click", async () => {
+    let link = await getRquest();
+
+    result.setAttribute("src", link);
 })
